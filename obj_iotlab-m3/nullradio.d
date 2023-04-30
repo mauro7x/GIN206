b38331bb956b4624b1ca856e6631420a7b8a3849
Mauro Parafati <parafati@srvssh>
2023-04-30 04:37:32 +0200

    add .gitignore

--- obj_iotlab-m3/nullradio.d
+++
@@ -1,6 +0,0 @@
-obj_iotlab-m3/nullradio.o: ../iot-lab/parts/contiki/core/dev/nullradio.c \
- ../iot-lab/parts/contiki/core/./dev/nullradio.h \
- ../iot-lab/parts/contiki/core/./dev/radio.h
-../iot-lab/parts/contiki/core/dev/nullradio.c :
- ../iot-lab/parts/contiki/core/./dev/nullradio.h :
- ../iot-lab/parts/contiki/core/./dev/radio.h :
